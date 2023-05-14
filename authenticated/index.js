86e2c544b5fbce7a05fa1e520dd50b3b25866582
sicotico <lpimperson@duck.com>
2023-05-14 16:58:31 +0200

    solucion de conflictos

--- authenticated/index.js
+++ authenticated/index.js
@@ -4,7 +4,6 @@ signOutBtn.addEventListener('click', () => {
     window.location.replace("/.auth/logout");
 });
 
-<<<<<<< HEAD
 
 const dataBtn = document.getElementById('data');
 
@@ -18,13 +17,3 @@ signOutBtn.addEventListener('click', () => {
       
       console.log(await getUserInfo());
 });
-=======
-async function getUserInfo() {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  };
-  
-  console.log(await getUserInfo());
->>>>>>> 67b003195285491729b7ac65b81340acfe369d8e
