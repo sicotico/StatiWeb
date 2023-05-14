b8671e3c06d4488bc9f896f40b62e949e837c905
sicotico <lpimperson@duck.com>
2023-05-14 16:02:58 +0200

    funcion obtener datos usuario

--- authenticated/index.js
+++ authenticated/index.js
@@ -3,3 +3,12 @@ const signOutBtn = document.getElementById('signOut');
 signOutBtn.addEventListener('click', () => {
     window.location.replace("/.auth/logout");
 });
+
+async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+  }
+  
+  console.log(await getUserInfo());
