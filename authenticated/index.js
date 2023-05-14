220bb089b74797b0930fdfab6b37ecb804325726
sicotico <lpimperson@duck.com>
2023-05-14 16:55:18 +0200

    Boton nuevo de datos de usuario

--- authenticated/index.js
+++ authenticated/index.js
@@ -4,11 +4,16 @@ signOutBtn.addEventListener('click', () => {
     window.location.replace("/.auth/logout");
 });
 
-async function getUserInfo() {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  }
-  
-  console.log(await getUserInfo());
+
+const dataBtn = document.getElementById('data');
+
+signOutBtn.addEventListener('click', () => {
+    async function getUserInfo() {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        return clientPrincipal;
+      }
+      
+      console.log(await getUserInfo());
+});
