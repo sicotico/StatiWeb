b27a2e2ba066b7eb5dc77726ce625f84eb5297d9
sicotico <lpimperson@duck.com>
2023-05-14 19:42:26 +0200

    Correcion de ruta auth

--- authenticated/index.js
+++ authenticated/index.js
@@ -9,7 +9,7 @@ const dataBtn = document.getElementById('data');
 data.addEventListener('click', () => {
   async function getUserInfo() {
     try {
-      const response = await fetch('.auth/me');
+      const response = await fetch('/.auth/me');
       const data = await response.json();
       console.log(data);
       document.getElementById('resultado').innerHTML = JSON.stringify(data);
